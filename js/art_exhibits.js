34ca74cd18e0636901c31f69bff2238b25bb21bf
000926085 <000926085@mohawkcollege.ca>
2025-02-14 17:12:33 -0500

    Added list of locations to use for markers.

---
+++ js/art_exhibits.js
@@ -0,0 +1,168 @@
+const art_exhibits = [
+    {
+        "X": 580957.7461,
+        "Y": 4787471.6172,
+        "OBJECTID": 1,
+        "NAME": "Griffin House National Historic Site",
+        "ADDRESS": "733 Mineral Springs Road",
+        "COMMUNITY": "Ancaster",
+        "WEBSITE": "https://www.hamilton.ca/attractions/hamilton-civic-museums/griffin-house-national-historic-site",
+        "LATITUDE": 43.235429,
+        "LONGITUDE": -80.002734
+    },
+    {
+        "X": 582829.9091,
+        "Y": 4786442.8191,
+        "OBJECTID": 2,
+        "NAME": "Fieldcote Memorial Park and Museum",
+        "ADDRESS": "64 Sulphur Springs Road",
+        "COMMUNITY": "Ancaster",
+        "WEBSITE": "https://www.hamilton.ca/attractions/hamilton-civic-museums/fieldcote-memorial-park-museum",
+        "LATITUDE": 43.226397,
+        "LONGITUDE": -79.979885
+    },
+    {
+        "X": 587378.6319,
+        "Y": 4779082.5315,
+        "OBJECTID": 3,
+        "NAME": "Canadian Warplane Heritage Museum",
+        "ADDRESS": "9280 Airport Road",
+        "COMMUNITY": "Glanbrook",
+        "WEBSITE": "https://www.warplane.com/",
+        "LATITUDE": 43.159947,
+        "LONGITUDE": -79.924899
+    },
+    {
+        "X": 590509.3629,
+        "Y": 4791200.4128,
+        "OBJECTID": 4,
+        "NAME": "The Hamilton Military Museum",
+        "ADDRESS": "610 York Boulevard",
+        "COMMUNITY": "Hamilton",
+        "WEBSITE": "https://www.hamilton.ca/attractions/hamilton-civic-museums/hamilton-military-museum",
+        "LATITUDE": 43.268528,
+        "LONGITUDE": -79.885009
+    },
+    {
+        "X": 591524.2442,
+        "Y": 4789998.8574,
+        "OBJECTID": 5,
+        "NAME": "Art Gallery of Hamilton",
+        "ADDRESS": "123 King Street West",
+        "COMMUNITY": "Hamilton",
+        "WEBSITE": "https://www.artgalleryofhamilton.com/",
+        "LATITUDE": 43.257328,
+        "LONGITUDE": -79.872378
+    },
+    {
+        "X": 594874.9934,
+        "Y": 4789443.3036,
+        "OBJECTID": 6,
+        "NAME": "Canadian Football Hall of Fame and Museum",
+        "ADDRESS": "64 Melrose Avenue North",
+        "COMMUNITY": "Hamilton",
+        "WEBSITE": "https://www.cfhof.ca/",
+        "LATITUDE": 43.251930,
+        "LONGITUDE": -79.830205
+    },
+    {
+        "X": 591559.9018,
+        "Y": 4789704.2525,
+        "OBJECTID": 7,
+        "NAME": "Whitehern Historic House and Garden National Historic Site",
+        "ADDRESS": "41 Jackson Street West",
+        "COMMUNITY": "Hamilton",
+        "WEBSITE": "https://www.hamilton.ca/attractions/hamilton-civic-museums/whitehern-historic-house-garden-national-historic-site",
+        "LATITUDE": 43.254780,
+        "LONGITUDE": -79.871947
+    },
+    {
+        "X": 595332.662,
+        "Y": 4788528.0416,
+        "OBJECTID": 8,
+        "NAME": "Hamilton Children's Museum",
+        "ADDRESS": "1072 Main Street East",
+        "COMMUNITY": "Hamilton",
+        "WEBSITE": "https://www.hamilton.ca/attractions/hamilton-civic-museums/hamilton-childrens-museum",
+        "LATITUDE": 43.243825,
+        "LONGITUDE": -79.825312
+    },
+    {
+        "X": 599675.6205,
+        "Y": 4790033.1433,
+        "OBJECTID": 9,
+        "NAME": "Hamilton Museum of Steam and Technology National Historic Site",
+        "ADDRESS": "900 Woodward Avenue",
+        "COMMUNITY": "Hamilton",
+        "WEBSITE": "https://www.hamilton.ca/attractions/hamilton-civic-museums/hamilton-museum-steam-technology-national-historic-site",
+        "LATITUDE": 43.256604,
+        "LONGITUDE": -79.772090
+    },
+    {
+        "X": 600218.2827,
+        "Y": 4785719.1253,
+        "OBJECTID": 10,
+        "NAME": "Hamilton Museum of Steam and Technology National Historic Site",
+        "ADDRESS": "77 King Street West",
+        "COMMUNITY": "Hamilton",
+        "WEBSITE": "https://www.hamilton.ca/attractions/hamilton-civic-museums/battlefield-house-museum-park-national-historic-site",
+        "LATITUDE": 43.257023,
+        "LONGITUDE": -79.870342
+    },
+    {
+        "X": 603795.7216,
+        "Y": 4784570.9866,
+        "OBJECTID": 11,
+        "NAME": "The Erland Lee Museum Home",
+        "ADDRESS": "552 Ridge Road",
+        "COMMUNITY": "Stoney Creek",
+        "WEBSITE": "https://fwio.on.ca/erland",
+        "LATITUDE": 43.206764,
+        "LONGITUDE": -79.722281
+    },
+    {
+        "OBJECTID": 12,
+        "NAME": "Earls Court Gallery",
+        "ADDRESS": "215 Ottawa Street North",
+        "COMMUNITY": "Hamilton",
+        "WEBSITE": "https://www.earlscourtgallery.ca/",
+        "LATITUDE": 43.248553,
+        "LONGITUDE": -79.817679
+    },
+    {
+        "OBJECTID": 13,
+        "NAME": "Crown and Press - A Julia Veenstra Gallery and Cafe",
+        "ADDRESS": "303 Ottawa Street North",
+        "COMMUNITY": "Hamilton",
+        "WEBSITE": "https://crownandpress.com/",
+        "LATITUDE": 43.250668,
+        "LONGITUDE": -79.816811
+    },
+    {
+        "OBJECTID": 14,
+        "NAME": "Hamilton Artists Inc.",
+        "ADDRESS": "155 James Street North",
+        "COMMUNITY": "Hamilton",
+        "WEBSITE": "https://www.theinc.ca/",
+        "LATITUDE": 43.260889,
+        "LONGITUDE": -79.867416
+    },
+    {
+        "OBJECTID": 15,
+        "NAME": "Josh Tiessen Studio Gallery",
+        "ADDRESS": "148 King Street East",
+        "COMMUNITY": "Stoney Creek",
+        "WEBSITE": "https://www.joshtiessen.com/",
+        "LATITUDE": 43.217293,
+        "LONGITUDE": -79.746830
+    },
+    {
+        "OBJECTID": 16,
+        "NAME": "Creations Art Gallery & Framing",
+        "ADDRESS": "436 Wilson Street East",
+        "COMMUNITY": "Ancaster",
+        "WEBSITE": "https://creations-gallery.com/",
+        "LATITUDE": 43.229687,
+        "LONGITUDE": -79.974379
+    }
+]
